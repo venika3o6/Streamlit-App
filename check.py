ed2461213ef70c22d2a561a1186eac89bf2cf65d
venika3o6 <venikam610@gmail.com>
2026-06-09 11:27:05 +0530

    Delete check.py

--- check.py
+++
@@ -1,8 +0,0 @@
-import streamlit as stm
-status=stm.radio("Select Gender:",['Male','Female'])
-if status=='Male':
-    stm.success('Male')
-else:
-    stm.success('Female')
-hobby=stm.selectbox("Select Hobby:", ['Reading', 'Writing', 'Drawing'])
-stm.write(f"Selected Hobby: {hobby}")
